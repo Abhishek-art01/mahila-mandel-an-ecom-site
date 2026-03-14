3a4993e2f22c268f94df16162d97f4874778e534
Abhishek Pandey <abhishekofficial4577@gmail.com>
2026-03-14 23:07:33 +0000

    add API dashboard html

--- server/server.js
+++ server/server.js
@@ -28,6 +28,8 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 connectDB();
